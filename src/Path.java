cd13e75bbf835c1b4e9766873193ec4c19cc3d7d
Liran-Barina <asdqwe4665>
2022-05-26 08:19:09 +0300

    Initial commit

---
+++ src/Path.java
@@ -0,0 +1,19 @@
+public class Path {
+    private int row; // Row = x
+    private int column; // Column = y
+
+    public Path(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+
+    public int getColumn() {
+        return column;
+    }
+
+}
